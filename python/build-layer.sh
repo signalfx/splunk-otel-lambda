494b45dcbac723871f545914b17678f1fea32095
Jakub Wach <jwach@splunk.com>
2021-12-21 09:33:39 +0100

    fix for python wrapper

--- python/build-layer.sh
+++ python/build-layer.sh
@@ -4,11 +4,13 @@ DISTRO_DIR=build
 SOURCES_DIR=otel/otel_sdk
 OTEL_PYTHON_DIR=../opentelemetry-lambda/python/src
 
-echo "Modify dependencies for Splunk integration"
+echo "Modify dependencies and script for Splunk integration"
 pushd "$OTEL_PYTHON_DIR/$SOURCES_DIR"
+
 sed -i 's/^opentelemetry-distro.*/splunk-opentelemetry[all]==1.2.0/g' requirements.txt
 sed -i 's/^opentelemetry-instrumentation.*//g' requirements.txt
 sed -i 's/^docker run --rm/docker run/g'  ../../build.sh
+sed -i 's/opentelemetry-instrument/splunk-py-trace/g'  otel-instrument
 
 echo "Modified python wrapper requirements:"
 cat requirements.txt
@@ -29,6 +31,7 @@ unzip -qo layer.zip && rm layer.zip
 rm -rf python/opentelemetry/exporter/jaeger python/opentelemetry-exporter-jaeger-thrift* python/thrift* python/six*
 
 mv otel-instrument otel-instrument-upstream
+
 popd
 # copy Splunk scripts (delegating to OTEL ones)
 cp otel-instrument $OTEL_PYTHON_DIR/$DISTRO_DIR/
