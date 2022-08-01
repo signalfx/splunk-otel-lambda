9bedabbfd8e293ac37f1a044758452259325bc77
John Bley <jbley@splunk.com>
2022-08-01 14:03:16 -0400

    Clean up debug output from java build

--- java/build-layer.sh
+++ java/build-layer.sh
@@ -33,9 +33,8 @@ cp ../scripts/* $OTEL_JAVA_DIR/$DISTRO_DIR/
 cd $OTEL_JAVA_DIR/$DISTRO_DIR
 echo "Performing manual size optimisations"
 pushd java/lib
-# UNUSED metrics
-# FIXME JB remove debug steps here
-ls -l
+# UNUSED metrics - seems to have disappeared from upstream?
+# FIXME Make this silent for now, revisit in the future
 rm -f opentelemetry-exporter-otlp-metrics-*.jar
 popd
 
