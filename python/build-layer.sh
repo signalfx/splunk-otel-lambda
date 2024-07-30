c5a2ec0d08f0af1904e434faf89a8ee842dae509
John Bley <jbley@splunk.com>
2024-07-30 12:05:59 -0400

    Add packging to python requirements to see if it fixes the build

--- python/build-layer.sh
+++ python/build-layer.sh
@@ -15,6 +15,8 @@ sed -i 's/0.45b0/0.45b0/g' nodeps-requirements.txt
 sed -i 's/0.45b0/0.45b0/g' requirements.txt
 sed -i 's/^docker run --rm/docker run/g'  ../../build.sh
 sed -i 's/opentelemetry-instrument/splunk-py-trace/g'  otel-instrument
+# FIXME this recently broke and why isn't it a vendored part of pkg_resources anymore? perhaps we should remove the dependency on pkg_resources?
+echo "packaging" >> requirements.txt
 echo "Modified python wrapper requirements:"
 cat requirements.txt
 cd ../..
