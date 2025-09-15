0a6f1291bb2c62686e0b38bb2dd3cde4d311f25e
John Bley <jbley@splunk.com>
2025-09-15 09:56:06 -0400

    adjust python versions and update to splunk-otel-python 2.7.0

--- python/build-layer.sh
+++ python/build-layer.sh
@@ -8,11 +8,11 @@ echo "Modify dependencies and script for Splunk integration"
 pushd "$OTEL_PYTHON_DIR"
 
 cd "$SOURCES_DIR"
-sed -i 's/^opentelemetry-distro.*/splunk-opentelemetry[all]==2.5.0/g' requirements.txt
+sed -i 's/^opentelemetry-distro.*/splunk-opentelemetry[all]==2.7.0/g' requirements.txt
 # Even if these regexes do nothing, leave these lines in to make later updates easier
-sed -i 's/0.56b0/0.56b0/g' nodeps-requirements.txt
-sed -i 's/0.56b0/0.56b0/g' requirements.txt
-sed -i 's/1.35.0/1.35.0/g' requirements.txt
+sed -i 's/0.57b0/0.57b0/g' nodeps-requirements.txt
+sed -i 's/0.57b0/0.57b0/g' requirements.txt
+sed -i 's/1.36.0/1.36.0/g' requirements.txt
 sed -i 's/^docker run --rm/docker run/g'  ../../build.sh
 sed -i '2isource /opt/splunk-default-config' otel-instrument
 
